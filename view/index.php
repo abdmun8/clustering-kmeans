ce4ff33ec0905e9d3e1cf27cae21840594338679
abdmun8@gmail.com <abdmun8@gmail.com>
2020-09-02 06:33:08 +0700

    add bootstrap, datatabble, login page

--- view/index.php
+++ view/index.php
@@ -1,2 +1,53 @@
-<?php
-echo "home";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cluster K-Means</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="./assets/css/dataTables.bootstrap.min.css" />
+
+</head>
+
+<body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Siswa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nilai</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kelas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Clustering</a>
+                </li>
+            </ul>
+            <div class="">
+                <span class="nav-item">Administrator</span>
+                <button class="btn btn-primary btn-sm ml-1">Logout</button>
+            </div>
+        </div>
+    </nav>
+    <section class="dynamic">
+        a
+    </section>
+
+
+    <script src="./assets/js/jquery-3.5.1.min.js"></script>
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/js/app.js"></script>
+    <script src="./assets/js/jquery.dataTables.min.js"></script>
+    <script src="./assets/js/dataTables.bootstrap.min.js"></script>
+</body>
+
+</html>
--- view/login.php
+++ view/login.php
@@ -1,2 +1,35 @@
-<?php
-echo "login";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cluster K-Means</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+
+</head>
+
+<body>
+    <section class="login-container">
+        <div class="login-form-container p-5">
+            <form class="form">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" id="username" placeholder="Masukan Username">
+                </div>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="password" class="form-control" id="username" placeholder="Masukan Password">
+                </div>
+                <button class="btn btn-primary btn-block">Login</button>
+            </form>
+        </div>
+    </section>
+
+    <script src="./assets/js/jquery-3.5.1.min.js"></script>
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/js/app.js"></script>
+</body>
+
+</html>
